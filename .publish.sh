3596fa4b3caa97af4b0efdabe7e670c1a252dd97
韩彦曦 <hanyanxi@ziggurat.cn>
2019-04-28 15:42:21 +0800

    update

---
+++ .publish.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -ev
+
+# get clone master
+git clone https://${GH_REF} .deploy_git
+cd .deploy_git
+git checkout master
+cd ../
+mv .deploy_git/.git/ ./public/
+cd ./public
+git config user.name "hyxkiller"
+git config user.email "546495041@qq.com"
+
+# add commit timestamp
+git add .
+git commit -m "Travis CI Auto Builder at `date +"%Y-%m-%d %H:%M"`"
+
+# Github Pages
+git push --force --quiet "https://${TravisCIToken}@${GH_REF}" master:master
